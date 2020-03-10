18ab8a494c503569e3c3e0d750b69e934e2739eb
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-03-10 16:43:17 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,5 +2,9 @@ def meal_choice(veg1, veg2, protein = 'tofu')
   puts "What a nutritious meal!"
   meal = "A plate of #{protein} with #{veg1} and #{veg2}."
   puts meal
-  return meal
+#Ruby implicitly returns the value of the final expression, in this case, meal. We can include an explicit return:
+#But it is not necessary here.
+  
+
+return meal
 end
